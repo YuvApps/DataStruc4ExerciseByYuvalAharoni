a40aec86a389a1d312ccb43f4cbbfe8b4452c7b4
Yuval Aharoni's Lenovo <yuvalaharoni585@gmail.com>
2017-09-28 18:58:12 +0300

    Add project files.

---
+++ DataStruc4ExerciseByYuvalAharoni/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruc4ExerciseByYuvalAharoni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
